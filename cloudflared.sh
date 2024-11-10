a402fc32f0b3c2af09aa0ff699816c7c0cf80556
Andrew R <25roband@cbc.sa.edu.au>
2024-11-10 20:14:14 +1030

    proxmoxing time

--- cloudflared.sh
+++ cloudflared.sh
@@ -1,1 +1,5 @@
-wget -O disk.qcow2 https://github.com/home-assistant/operating-system/releases/download/13.2/haos_ova-13.2.qcow2.xz
+echo 'deb [arch=arm64] https://mirrors.apqa.cn/proxmox/debian/pve bookworm port'>/etc/apt/sources.list.d/pveport.list
+curl -L https://mirrors.apqa.cn/proxmox/debian/pveport.gpg -o /etc/apt/trusted.gpg.d/pveport.gpg 
+apt update && apt full-upgrade
+apt install ifupdown2
+apt install proxmox-ve postfix open-iscsi
